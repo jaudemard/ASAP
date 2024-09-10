4b1bcdff8b9622e979d975751c8c16be40f4803d
jaudemard <juliette.aud@live.fr>
2024-09-10 17:20:27 +0200

    better handling of accessibility attribute

--- asap/shrake_rupley.py
+++ asap/shrake_rupley.py
@@ -3,40 +3,49 @@
 from classes import Protein, Sphere
 import numpy as np
 
-
+sonde = 1.52
 
 PROTEIN = Protein("toy/1a5d.pdb")
 
 counter = 0
 
-print(len(PROTEIN.list_atoms),"****")
+print(len(PROTEIN.atoms),"****")
 
-for atom in PROTEIN.list_atoms:
+
+
+for atom in PROTEIN.atoms:
     sphere = Sphere(n=92) 
-    sphere.scale_and_move(atom.coord, atom.radius)
+    sphere.scale_and_move(atom.coord, (atom.radius+sonde))
 
-    atom.connectivity(PROTEIN.list_atoms, 5)
+    atom.connectivity(PROTEIN.atoms, 15)
 
-
+    accessible_point = 0
     for point in sphere.lattice:
+
         for neighbour in atom.neighbour:
             if neighbour.id == atom.id: continue
 
             dist = neighbour.distance(point.coord)
 
-            if dist < (neighbour.radius + (1.52*2)):
+
+
+            if dist < (neighbour.radius + (1.52)):
                 point.set_accessibility(False)
-            else:
-                point.set_accessibility(True)
+                break
 
-    accessible_point = [point.accessibility for point in sphere.lattice].count(True)
+        if point.accessibility == True:
+            accessible_point += 1
 
-    area = 4 * np.pi * (atom.radius)**2
+    area = 4 * np.pi * (atom.radius+sonde)**2
 
     cover_factor = area / 92
 
     atom.accessibility = accessible_point * cover_factor
 
-list_access = [a.accessibility for a in PROTEIN.list_atoms]
+    
+
+list_access = [a.accessibility for a in PROTEIN.atoms]
+
+
 
 print(sum(list_access))
