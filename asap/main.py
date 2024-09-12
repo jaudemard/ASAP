89cf1ba42ae19ed97a2ec994fa62f100131bc8e2
jaudemard <juliette.aud@live.fr>
2024-09-12 14:44:15 +0200

    use argparse because docopt is a mess

--- asap/main.py
+++ asap/main.py
@@ -1,27 +1,47 @@
-"""
-usage:
-    asap <command> [<args>...]
-
-options:
-    -h --help  Show help
-commands:
-    shrake_rupley   Determine Solvent Accessible Surface Area with a Shrake and Rupley based method.
-"""
-import docopt
+import argparse
 import importlib
 import sys
 
-def main(args=None):
-    args = docopt.docopt(__doc__)
+def main():
+    parser = argparse.ArgumentParser(description='Compute Solvent Accessible Surface Area of a Protein.')
 
-    # Extract command
-    command = args.pop('<command>')
-    # Extract options and argument
-    command_args = args.pop('<args>')
+    # Define the command-line arguments
+    parser.add_argument('command', type=str, help='Algorithm to compute SASA: shrake_rupley')
+    parser.add_argument('--pdb', type=str, help='Path to the PDB structure')
+    parser.add_argument('--probe', type=float, help='Size of the probe')
+    parser.add_argument('--output', type=str, default='.', help='Output directory for the output files')
+    parser.add_argument('--model', type=int, default=0, help='Model used if the pdb includes several of them')
+    parser.add_argument('--point', type=int, default=100, help='Number of points in the sphere lattice')
+    
+    args = parser.parse_args()
 
-    if command == "shrake_rupley":
-        import shrake_rupley
-        shrake_rupley.shrake_rupley_cli(command_args)
+    # Extract command and arguments
+    command = args.command
+    command_args = [
+        '--pdb', str(args.pdb),
+        '--probe', str(args.probe),
+        '--output', str(args.output),
+        '--model', str(args.model),
+        '--point', str(args.point)
+    ]
+    try:
+        command_module = importlib.import_module(f"asap.{command}")
+    except ModuleNotFoundError:
+        print(f"Unrecognized command '{command}'")
+        parser.print_help()
+        sys.exit(1)
+
+    # Build the CLI function name
+    cli_function_name = f"{command}_cli"
+    if not hasattr(command_module, cli_function_name):
+        print(f"No CLI function '{cli_function_name}' found in module '{command}'")
+        sys.exit(1)
+
+    cli_function = getattr(command_module, cli_function_name)
+
+    # Execute the CLI function with command_args
+    cli_function(command_args)
+
 
 if __name__ == '__main__':
     main()
